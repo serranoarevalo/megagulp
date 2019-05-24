70b10ec45bbb27a747a3cab76e5cf399d06c4f1a
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2019-05-24 16:18:41 +0900

    Task Planning

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,21 @@
 import gulp from "gulp";
+
+const routes = {
+  css: {},
+  pug: {},
+  js: {}
+};
+
+function js() {}
+
+function styles() {}
+
+function watch() {}
+
+function pug() {}
+
+function clean() {}
+
+function server() {}
+
+export function img() {}
